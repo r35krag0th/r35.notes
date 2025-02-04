4ef3f68818a4d1df6ab05f2473a1f7139d30d280
Bob Saska <git@r35.net>
2025-02-04 16:00:09 -0600

    fix: fixed bug with keybinds and usercommands

--- lazy.lua
+++ lazy.lua
@@ -5,4 +5,15 @@ return {
     "nvim-telescope/telescope.nvim",
     "echasnovski/mini.pick",
   },
+  keys = {
+    { "<localleader>nd", "<Cmd>RNToday<CR>", desc = "Go to Today" },
+    { "<localleader>ny", "<Cmd>RNYesterday<CR>", desc = "Go to Yesterday" },
+    { "<localleader>nt", "<Cmd>RNTomorrow<CR>", desc = "Go to Tomorrow" },
+    { "<localleader>ng", "<Cmd>RNGrep<CR>", desc = "Grep Notes" },
+    { "<localleader>nf", "<Cmd>RNFind<CR>", desc = "Find Note" },
+    { "<localleader>np", "<Cmd>RNPrevious<CR>", desc = "Go to Previous Note" },
+    { "<localleader>nP", "<Cmd>RNPreviousSkipEmpty<CR>", desc = "Go to Previous Note (Skip Empty)" },
+    { "<localleader>nn", "<Cmd>RNNext<CR>", desc = "Go to Next Note" },
+    { "<localleader>nN", "<Cmd>RNNextSkipEmpty<CR>", desc = "Go to Next Note (Skip Empty)" },
+  },
 }
--- lua/r35-notes/init.lua
+++ lua/r35-notes/init.lua
@@ -10,6 +10,10 @@ local M = {}
 ---@field tomorrow string
 ---@field find string
 ---@field grep string
+---@field previous_no_skip string
+---@field previous_with_skip string
+---@field next_no_skip string
+---@field next_with_skip string
 
 ---@class r35NotesOptions
 ---@field root string
@@ -20,6 +24,10 @@ M.opts = {
     yesterday = "<localleader>ny",
     today = "<localleader>nd",
     tomorrow = "<localleader>nt",
+    previous_no_skip = "<localleader>np",
+    previous_with_skip = "<localleader>nP",
+    next_no_skip = "<localleader>nn",
+    next_with_skip = "<localleader>nN",
     find = "<localleader>nf",
     grep = "<localleader>ng",
   },
@@ -32,17 +40,19 @@ end
 
 ---Internal options for the plugin.
 ---@class r35NotesLocalOptions
----@field today_command string
----@field yesterday_command string
----@field tomorrow_command string
----@field find_command string
----@field grep_command string
+---@field commands r35NotesKeymap
 local local_opts = {
-  today_command = "RNToday",
-  yesterday_command = "RNYesterday",
-  tomorrow_command = "RNTomorrow",
-  find_command = "RNFind",
-  grep_command = "RNGrep",
+  commands = {
+    today = "RNToday",
+    yesterday = "RNYesterday",
+    tomorrow = "RNTomorrow",
+    previous_no_skip = "RNPrevious",
+    previous_with_skip = "RNPreviousSkipEmpty",
+    next_no_skip = "RNNext",
+    next_with_skip = "RNNextSkipEmpty",
+    find = "RNFind",
+    grep = "RNGrep",
+  },
 }
 
 ---Find files in the notes directory using mini.pick
@@ -87,19 +97,23 @@ local date_to_journal = function(date)
 end
 
 ---Find the nearest weekday to the current date starting with `days` offset.
----@param days number
-local relative_to_journal = function(days)
-  local now = os.time()
-  now = now + ((24 * 60 * 60) * days)
-  local d = os.date("*t", now)
+---@param days integer
+---@param start_at integer|nil
+local relative_to_journal = function(days, start_at)
+  if start_at == nil then
+    start_at = os.time()
+  end
+
+  start_at = start_at + ((24 * 60 * 60) * days)
+  local d = os.date("*t", start_at)
   while d.wday == 1 or d.wday == 7 do
     -- roll another day
     if days <= 0 then
-      now = now - (24 * 60 * 60)
+      start_at = start_at - (24 * 60 * 60)
     else
-      now = now + (24 * 60 * 60)
+      start_at = start_at + (24 * 60 * 60)
     end
-    d = os.date("*t", now)
+    d = os.date("*t", start_at)
   end
 
   date_to_journal(d)
@@ -156,19 +170,94 @@ M.setup = function(opts)
   end
 
   -- Setup the User Commands
-  vim.api.nvim_create_user_command(local_opts.yesterday_command, M.goto_yesterday, { nargs = 0 })
-  vim.api.nvim_create_user_command(local_opts.today_command, M.goto_today, { nargs = 0 })
-  vim.api.nvim_create_user_command(local_opts.tomorrow_command, M.goto_tomorrow, { nargs = 0 })
-  vim.api.nvim_create_user_command(local_opts.find_command, M.find, { nargs = 0 })
-  vim.api.nvim_create_user_command(local_opts.grep_command, M.grep, { nargs = 0 })
+  vim.api.nvim_create_user_command(local_opts.commands.yesterday, M.goto_yesterday, {
+    nargs = 0,
+    desc = "Open yesterday's journal",
+  })
+  vim.api.nvim_create_user_command(local_opts.commands.today, M.goto_today, {
+    nargs = 0,
+    desc = "Open today's journal",
+  })
+  vim.api.nvim_create_user_command(local_opts.commands.tomorrow, M.goto_tomorrow, {
+    nargs = 0,
+    desc = "Open tomorrow's journal",
+  })
+  vim.api.nvim_create_user_command(local_opts.commands.find, M.find, {
+    nargs = 0,
+    desc = "Find a journal file",
+  })
+  vim.api.nvim_create_user_command(local_opts.commands.grep, M.grep, {
+    nargs = 0,
+    desc = "Grep through journals",
+  })
+  vim.api.nvim_create_user_command(local_opts.commands.previous_no_skip, M.goto_previous_day, {
+    nargs = 0,
+    desc = "Previous journal relative to the current buffer",
+  })
+  -- vim.api.nvim_create_user_command(local_opts.commands.previous_with_skip, M.goto_previous_day, { nargs = 0 })
+  vim.api.nvim_create_user_command(local_opts.commands.next_no_skip, M.goto_next_day, {
+    nargs = 0,
+    desc = "Next journal relative to the current buffer",
+  })
+  -- vim.api.nvim_create_user_command(local_opts.commands.next_with_skip, M.goto_next_day, { nargs = 0 })
 
   -- Setup Keymaps
-  -- TODO: Make these use callback.
-  vim.api.nvim_set_keymap("n", M.opts.keymap.yesterday, keymap_string(local_opts.yesterday_command), {})
-  vim.api.nvim_set_keymap("n", M.opts.keymap.today, keymap_string(local_opts.today_command), {})
-  vim.api.nvim_set_keymap("n", M.opts.keymap.tomorrow, keymap_string(local_opts.tomorrow_command), {})
-  vim.api.nvim_set_keymap("n", M.opts.keymap.find, keymap_string(local_opts.find_command), {})
-  vim.api.nvim_set_keymap("n", M.opts.keymap.grep, keymap_string(local_opts.grep_command), {})
+  -- for i, v in pairs(M.opts.keymap) do
+  --   vim.api.nvim_set_keymap("n", v, keymap_string(local_opts.commands[i]), {})
+  -- end
+end
+
+local current_file = function()
+  local absolute_path = vim.api.nvim_buf_get_name(0)
+  local match = absolute_path:match("(?P<year>d+)[/\\](?P<month>d+)[/\\](?P<day>d+).md$")
+
+  if match then
+    return {
+      year = tonumber(match.year),
+      month = tonumber(match.month),
+      day = tonumber(match.day),
+    }
+  end
+
+  return ""
+end
+
+M.goto_previous_day = function()
+  local current = current_file()
+  if current == "" then
+    vim.notify("The current file doesn't seem to be a journal file.")
+    return
+  end
+
+  local previous = os.time({
+    year = current.year,
+    month = current.month,
+    day = current.day,
+    hour = 0,
+    min = 0,
+    sec = 0,
+  }) - (24 * 60 * 60)
+
+  relative_to_journal(-1, previous)
+end
+
+M.goto_next_day = function()
+  local current = current_file()
+  if current == "" then
+    vim.notify("The current file doesn't seem to be a journal file.")
+    return
+  end
+
+  local next = os.time({
+    year = current.year,
+    month = current.month,
+    day = current.day,
+    hour = 0,
+    min = 0,
+    sec = 0,
+  }) + (24 * 60 * 60)
+
+  relative_to_journal(1, next)
 end
 
 return M
